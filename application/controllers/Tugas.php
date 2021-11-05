3dedc802794817a76eacbd8c6729e3b56262e4c0
Pahrul Irfan <ip.5090@gmail.com>
2021-11-05 16:07:38 +0800

    tugas

---
+++ application/controllers/Tugas.php
@@ -0,0 +1,9 @@
+<?php
+
+class Tugas extends CI_Controller
+{
+	public function kedua()
+	{
+		return $this->load->view('admin/v_index');
+	}
+}
--- application/views/admin/v_index.php
+++ application/views/admin/v_index.php
@@ -5,17 +5,45 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Backend</title>
+	<title>Data</title>
 	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
 </head>
 
 <body>
 
-	<div class="container">
-		<h2>backend</h2>
+<div class="container mt-3">
+	<h2 class="text-center alert alert-info">Tugas 2</h2>
+	<div class="row">
+		<div class="col-6">
+			<div class="card card-body">
+				<table class="table table-bordered">
+					<thead>
+					<tr>
+						<td>Nim</td>
+						<td>Nama</td>
+						<td>Alamat</td>
+					</tr>
+					</thead>
+					<tbody>
+					<tr>
+						<td>nim_anda</td>
+						<td>nama_anda</td>
+						<td>alamat_anda</td>
+					</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="col-3">
+			<div class="card card-body">
+				<img src="https://placeimg.com/200/140/people" alt="">
+				ganti dengan foto anda
+			</div>
+		</div>
 	</div>
+</div>
 
-	<script src="<?php echo base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/bootstrap.bundle.min.js'); ?>"></script>
 </body>
 
 </html>
